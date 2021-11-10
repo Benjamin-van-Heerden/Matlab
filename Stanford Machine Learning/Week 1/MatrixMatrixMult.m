bcba93793da04cf19b94b6bce95d5c993d746f60
Benjamin-van-Heerden <benjaminvh1997@gmail.com>
2021-11-10 10:23:27 +0200

    Completed week 1 of Stanford Machine Learning

---
+++ Stanford Machine Learning/Week 1/MatrixMatrixMult.m
@@ -0,0 +1,13 @@
+M1 = [1, 3, 2; 4, 0, 1]
+M2 = [1, 3;, 0, 1;, 5, 2]
+
+Prod = M1 * M2
+
+% Initialize a 3 by 2 matrix 
+A = [1, 2; 3, 4;5, 6]
+
+% Initialize a 2 by 1 matrix 
+B = [1; 2] 
+
+% We expect a resulting matrix of (3 by 2)*(2 by 1) = (3 by 1) 
+mult_AB = A*B
